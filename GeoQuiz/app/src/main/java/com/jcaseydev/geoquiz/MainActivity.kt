9be8162e4fee4b1b3e9db0f411c8673fab7474cf
jmcs811 <justincaseyy69@gmail.com>
2019-10-27 14:09:33 -0400

    Added savedInstanceState

--- GeoQuiz/app/src/main/java/com/jcaseydev/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/jcaseydev/geoquiz/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jcaseydev.geoquiz
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
@@ -10,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +29,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -99,5 +105,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun showGrade() {
         Toast.makeText(this, quizViewModel.calcGrade().toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        Log.i(TAG, "onSavedInstanceState")
+        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
     }
 }
